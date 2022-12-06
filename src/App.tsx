ffddb365a4942a6391dbea5d71b677791ecd887d
Brett Sherwood <Brett.Sherwood@generac.com>
2022-12-05 18:53:40 -0800

    initial build

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div style={{ padding: '16px' }}>
+      <h1>lyric.ly</h1>
+      <textarea></textarea>
+    </div>
+  );
+};
