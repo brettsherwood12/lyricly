55affe13a3d4fa229d3ba6aa9e75edec31fed425
Brett Sherwood <Brett.Sherwood@generac.com>
2023-01-30 20:25:15 -0800

    improve about page layout

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,21 +1,12 @@
-import React, { useContext } from 'react';
-import { Box, Divider, Typography } from '@mui/material';
+import React from 'react';
+import { Box, Divider } from '@mui/material';
 
-import { Context } from '../App';
 import { Lyrics } from '../components/Lyrics';
 import { Results } from '../components/Results';
 
-const boxSx = {
-  height: '100%',
-  display: 'flex',
-  gap: '24px',
-};
-
 export const Home = () => {
-  const { selectedLyric } = useContext(Context);
-
   return (
-    <Box sx={boxSx}>
+    <Box sx={{ height: '100%', display: 'flex', gap: '24px' }}>
       <Box sx={{ width: '50%' }}>
         <Lyrics />
       </Box>
