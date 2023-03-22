721f00f344cce438cbec38cb21d8df34c34cf199
Brett Sherwood <Brett.Sherwood@generac.com>
2023-03-21 19:22:19 -0700

    restyle dialogs

--- src/components/DeleteDialog.tsx
+++ src/components/DeleteDialog.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Box, Button, Dialog, Typography } from '@mui/material';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
 
 import type { Dispatch, SetStateAction } from 'react';
 
@@ -11,26 +18,23 @@ interface Props {
 
 export const DeleteDialog = ({ isOpen, setIsOpen, handleDelete }: Props) => {
   return (
-    <Dialog open={isOpen} maxWidth="sm" fullWidth>
-      <Box sx={{ p: 3 }}>
-        <Box pb={2}>
-          <Typography gutterBottom>
-            Are you sure you want to delete your saved lyrics? This action is irreversible.
-          </Typography>
-          <Typography gutterBottom>
-            This action deletes your lyric's save record only, it <strong>will not</strong> affect
-            the text in the editor.
-          </Typography>
-        </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Button variant="contained" size="small" color="warning" onClick={() => setIsOpen(false)}>
-            Cancel
-          </Button>
-          <Button variant="contained" size="small" color="error" onClick={() => handleDelete()}>
-            Delete
-          </Button>
-        </Box>
-      </Box>
+    <Dialog open={isOpen}>
+      <DialogTitle>Delete Saved Lyrics</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Are you sure you want to permanently delete your saved lyrics? This action only deletes
+          the record of your saved lyrics, it <strong>will not</strong> affect the text in the
+          editor.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" color="warning" onClick={() => setIsOpen(false)}>
+          Cancel
+        </Button>
+        <Button variant="contained" color="error" onClick={() => handleDelete()} autoFocus>
+          Delete
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
--- src/components/LoadDialog.tsx
+++ src/components/LoadDialog.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Box, Button, Dialog, Typography } from '@mui/material';
+// import { Box, Button, Dialog, Typography } from '@mui/material';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
 
 import type { Dispatch, SetStateAction } from 'react';
 
@@ -11,26 +19,22 @@ interface Props {
 
 export const LoadDialog = ({ isOpen, setIsOpen, handleLoad }: Props) => {
   return (
-    <Dialog open={isOpen} maxWidth="sm" fullWidth>
-      <Box sx={{ p: 3 }}>
-        <Box pb={2}>
-          <Typography gutterBottom>
-            Are you sure you want to load your saved lyrics? This action is irreversible.
-          </Typography>
-          <Typography gutterBottom>
-            This action loads your lyric's save record, and it will cause the text in the editor to
-            be lost.
-          </Typography>
-        </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Button variant="contained" size="small" color="warning" onClick={() => setIsOpen(false)}>
-            Cancel
-          </Button>
-          <Button variant="contained" size="small" onClick={() => handleLoad()}>
-            Load
-          </Button>
-        </Box>
-      </Box>
+    <Dialog open={isOpen}>
+      <DialogTitle>Load Saved Lyrics</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Are you sure you want to load your saved lyrics? This action will{' '}
+          <strong>permanently</strong> discard any changes you have made in the text editor.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" color="warning" onClick={() => setIsOpen(false)}>
+          Cancel
+        </Button>
+        <Button variant="contained" onClick={() => handleLoad()} autoFocus>
+          Load
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
